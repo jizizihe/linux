61f24f6050dc4604086523632ddb44d8a9d49a21
jzzh <jzzh@szbaijie.cn>
2020-08-13 11:16:01 +0800

    change some

--- vim/ctags_cscope.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-find . -name "*.h" -o -name "*.c" -o -name "*.cc"  -o -name "*.java"> cscope.files
-cscope -Rbkq -i cscope.files
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q
-#ctags -R .
