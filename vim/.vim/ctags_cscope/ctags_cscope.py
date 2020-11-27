f00ee701cad7c8b429eddcaf6d080c3e900fc33a
jzzh <jzzh@szbaijie.cn>
2020-11-27 11:36:36 +0800

    filter c java cpp... for cscope

--- vim/.vim/ctags_cscope/ctags_cscope.py
+++ vim/.vim/ctags_cscope/ctags_cscope.py
@@ -9,12 +9,14 @@
 import os
 
 save_file = open("cscope.files", 'w')
+filter=[".java", ".h", ".c", ".cc", ".cpp", ".py", ".sh"]
 
 for root, dirs, list in os.walk("./"):
     for i in list:
         cur_path = os.path.abspath(root)
         file_path = cur_path + '/' + i
-        
-        save_file.write(file_path+'\n')
+        ext = os.path.splitext(file_path)[1]
+        if ext in filter:
+            save_file.write(file_path+'\n')
         
 save_file.close()
